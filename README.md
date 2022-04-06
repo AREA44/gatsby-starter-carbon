2999d40f8464e6888fde0a18849099079e534a28
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-04-06 10:55:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Gatsby Starter Carbon
+
+## What is this?
+
+> This is a starter-kit (boilerplate) with a dependancy on the `gatsby-theme-carbon`
+> package. The primary goal of `gatsby-theme-carbon` is to get content authors
+> speaking the IBM Design Language with Carbon as soon as possible. It includes
+> some sample components/content demos in the `src/pages` directory.
+
+## How do I use it?
+
+Check out our quick
+[getting started](https://gatsby.carbondesignsystem.com/getting-started) guide and
+video!
+
+## License
+
+The licensed under the [MIT](LICENSE) license.
---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter Carbon',
+    description: 'This is a starter-kit (boilerplate) with a dependancy on the carbon design system',
+    keywords: 'gatsby, theme, carbon, IBM',
+  },
+  pathPrefix: `/gatsby-start-carbon`,
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gatsby Starter Carbon',
+        icon: 'src/images/favicon.svg',
+        short_name: 'Gatsby Theme Carbon',
+        start_url: '/',
+        display: 'browser',
+      },
+    },
+    {
+      resolve: 'gatsby-theme-carbon',
+      options: {
+        navigationStyle: 'header',
+      },
+    },
+  ],
+};
